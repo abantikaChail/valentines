2f4604207395d255a775a2970042543bf1dfc114
abantikaChail <69887436+abantikaChail@users.noreply.github.com>
2024-02-12 23:28:39 +0530

    Add files via upload

---
+++ valentine.js
@@ -0,0 +1,17 @@
+document.getElementById('No').addEventListener('mouseover', function() {
+    this.classList.add('move');
+    document.getElementById('Yes').classList.remove('move');
+});
+
+document.getElementById('Yes').addEventListener('mouseover', function() {
+    this.classList.add('move');
+    document.getElementById('No').classList.remove('move');
+});
+
+document.getElementById('No').addEventListener('click', function(e) {
+    e.preventDefault();
+});
+
+document.getElementById('Yes').addEventListener('click', function(e) {
+    e.preventDefault();
+});
