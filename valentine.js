fa6d9f26b8fbc5e7fdd614257911c5cd5a714607
abantikaChail <69887436+abantikaChail@users.noreply.github.com>
2024-02-12 23:34:55 +0530

    Update valentine.js

--- valentine.js
+++ valentine.js
@@ -3,15 +3,15 @@ document.getElementById('No').addEventListener('mouseover', function() {
     document.getElementById('Yes').classList.remove('move');
 });
 
-document.getElementById('Yes').addEventListener('mouseover', function() {
-    this.classList.add('move');
-    document.getElementById('No').classList.remove('move');
-});
+// document.getElementById('Yes').addEventListener('mouseover', function() {
+//     this.classList.add('move');
+//     document.getElementById('No').classList.remove('move');
+// });
 
 document.getElementById('No').addEventListener('click', function(e) {
     e.preventDefault();
 });
 
-document.getElementById('Yes').addEventListener('click', function(e) {
-    e.preventDefault();
-});
+// document.getElementById('Yes').addEventListener('click', function(e) {
+    // e.preventDefault();
+// });
